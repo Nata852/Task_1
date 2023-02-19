935c9a45a62b9738d3c26ecf9e1bf5771d990930
Natali <natlyly3@mail.ru>
2023-02-19 16:51:36 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int sentr2(int number)
+{
+    int  numberFirst = number /10;
+    int resalt = numberFirst % 10;
+    return resalt;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{sentr2(number)}");
+
